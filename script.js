2c63f8e5899e687931fb7e3544426dd64591647a
StoleB <stole.boskov@gmail.com>
2024-07-23 00:00:10 +0200

    Update

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ document.querySelector('.prev').addEventListener('click', rotate);
 
 function rotate(e) {
     if (e.target.className =='next') {
-        currdeg = currdeg -60;
+        currdeg = currdeg +60;
     }
     if (e.target.className =='prev') {
-        currdeg = currdeg +60;
+        currdeg = currdeg -60;
     }
     document.querySelector('.items').style.transform='rotateY('+currdeg + 'deg)';
 }
